92bb8c3cd3491161674839f14283702929cf9b1d
Imane Jahid <imane.jahid456@gmail.com>
2025-05-12 00:26:19 -0700

    Modified publisher python file

--- publisher.py
+++ publisher.py
@@ -2,6 +2,11 @@ import pandas as pd
 import requests
 import json
 from google.cloud import pubsub_v1
+from concurrent import futures
+
+def future_callback(future):
+    try: future.result() # Wait for result of publish operation.
+    except Exception as e: print(f"An error occurred: {e}")
 
 # Set your GCP project ID and topic name
 project_id = "data-engineering-ij-indiv"
@@ -13,6 +18,7 @@ topic_path = publisher.topic_path(project_id, topic_id)
 
 # Load the Excel file
 df = pd.read_excel("vehicle_ids.xlsx")
+future_list = [] # This future_list will allow us to create a data structure to inspect in a blocking for loop shortly.
 
 # Extract the vehicle IDs from the "Titan" column
 vehicle_ids = df['Titan'].dropna().astype(str)
@@ -30,6 +36,8 @@ for vehicle_id in vehicle_ids:
             for breadcrumb in data:
                 message_data = json.dumps(breadcrumb).encode("utf-8")
                 future = publisher.publish(topic_path, data=message_data)
+                future.add_done_callback(future_callback)     # This is the callback we use to essentially acknowledge the result.
+                future_list.append(future)    # Append this new future to our list of futures.
                 print(f"Published breadcrumb for vehicle {vehicle_id}")
 
         except Exception as e:
@@ -37,3 +45,5 @@ for vehicle_id in vehicle_ids:
     else:
         print(f"Failed to fetch for {vehicle_id} (Status: {response.status_code})")
 
+for future in futures.as_completed(future_list): 
+    continue
